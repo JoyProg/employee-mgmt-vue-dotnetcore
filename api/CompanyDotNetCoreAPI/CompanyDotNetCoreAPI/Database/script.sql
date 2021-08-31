bb5cd6f1ad37b6a4fa1b09104dd9bdee712384a5
JoyProg <joiprogrammingemail@gmail.com>
2021-08-31 15:35:44 +0100

    Initial Commit

---
+++ api/CompanyDotNetCoreAPI/CompanyDotNetCoreAPI/Database/script.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[Department](
+	[DepartmentId] [int] IDENTITY(1,1) NOT NULL,
+	[DepartmentName] [varchar](500) NULL
+) 
+
+INSERT INTO [dbo].[Department]
+VALUES ('IT'),
+('Support')
+
+CREATE TABLE [dbo].[Department](
+	[DepartmentId] [int] IDENTITY(1,1) NOT NULL,
+	[DepartmentName] [varchar](500) NULL
+)
+
+INSERT INTO [dbo].[Department]
+VALUES ('IT'),
+('Support')
---
+++ ui/variables.js
@@ -0,0 +1,4 @@
+const variables = {
+    API_URL:"http://localhost:52719/api/",
+    PHOTO_UTL:"http://localhost:52719/photos/"
+}
